87b37ad093ad1b5d1896b5d190de73b60f338542
Krybe12 <Krybe120@gmail.com>
2021-04-16 07:53:59 +0200

    formating fix

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
 require 'include/conn.php';
-if (isset($_POST["id"])){
+if (isset($_POST["id"]) and is_numeric($_POST["id"])){
   $stmt = $conn->prepare("DELETE FROM people WHERE id=:id");
   $stmt->execute([':id' => $_POST['id']]);
   $stmt = null;
--- save.php
+++ save.php
@@ -1,6 +1,6 @@
 <?php
 require "include/conn.php";
-if (!isset($_POST["fname"])) {header('Location: index.php');}
+if (!isset($_POST["fname"])) header('Location: index.php');
 
 $stmt = $conn->prepare("INSERT INTO people VALUES (null, :firstname, :lastname, :age)");
 $stmt->execute([":firstname" => $_POST["fname"], ":lastname" => $_POST["lname"], ":age" => $_POST["age"]]);
